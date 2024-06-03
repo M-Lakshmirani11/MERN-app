103da1339ba56986a9b72e2b684d668b002bf4ea
M-Lakshmirani11 <166198606+M-Lakshmirani11@users.noreply.github.com>
2024-06-04 00:03:43 +0530

    error.js

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,27 +1,22 @@
 class ErrorHandler extends Error {
-    constructor(message, statusCode) {
+    constructor(message, statuscode) {
         super(message);
-        this.statusCode = statusCode;
+        this.statusCode = statuscode;
     }
 }
-
 export const errorMiddleware = (err, req, res, next) => {
     err.message = err.message || "Internal Server Error";
-    err.statusCode = err.statusCode || 500;
+    err.statusCode = err.statucode || 500;
 
     if (err.name === "CastError") {
         const message = `Resource not found. Invalid: ${err.path}`;
         err = new ErrorHandler(message, 400);
     }
-
-
     if (err.name === 'ValidationError') {
         const validationErrors = Object.values(error.errors).map(err => err.message);
         return next(new ErrorHandler(validationErrors.join(', '), 400));
     }
-
-
-    return res.status(err.statusCode).json({
+    return res.status(err.statuscode).json({
         success: false,
         message: err.message,
     });
